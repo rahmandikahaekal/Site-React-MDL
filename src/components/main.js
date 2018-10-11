2c2b93ca91798eb9dd811cc83186fb5093793759
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2018-10-11 07:29:23 +0700

    create my site react

---
+++ src/components/main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    Switch,
+    Route
+} from 'react-router-dom';
+import LandingPage from './landingpage';
+import AboutMe from './aboutme';
+import Resume from './resume';
+import Project from './project';
+import Contact from './contact';
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/project" component={Project} />
+        <Route path="/contact" component={Contact} />
+    </Switch>
+)
+
+export default Main;
